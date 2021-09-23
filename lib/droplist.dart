3b3632530cd40251f8643f5e61a6b00c7876264e
Greg Jarmiolowski <greg@sqlprompt.net>
2021-09-22 23:57:11 -0400

    added final for immutable class

--- lib/droplist.dart
+++ lib/droplist.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class DropdownList extends StatefulWidget {
   //String chosenValue;
-  List<String> itemsList;
+  final List<String> itemsList;
   DropdownList(this.itemsList);
   @override
   _DropdownState createState() => _DropdownState(itemsList);
@@ -16,32 +16,30 @@ class _DropdownState extends State<DropdownList> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: const EdgeInsets.all(0.0),
-        child: DropdownButton<String>(
-          value: _itemsList[0],
-          //elevation: 5,
-          style: TextStyle(color: Colors.black),
+      padding: const EdgeInsets.all(0.0),
+      child: DropdownButton<String>(
+        value: _itemsList[0],
+        //elevation: 5,
+        style: TextStyle(color: Colors.black),
 
-          items: _itemsList.map<DropdownMenuItem<String>>((String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(value),
-            );
-          }).toList(),
-          hint: Text(
-            "Please choose a langauage",
-            style: TextStyle(
-                color: Colors.black,
-                fontSize: 16,
-                fontWeight: FontWeight.w600),
-          ),
-          onChanged: (String? value) => setState(() {
-            if (value!.isNotEmpty) {
-              this._chosenValue = value;
-              print('set value to: ' + value);
-            }
-            }),
+        items: _itemsList.map<DropdownMenuItem<String>>((String value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(value),
+          );
+        }).toList(),
+        hint: Text(
+          "Please choose a langauage",
+          style: TextStyle(
+              color: Colors.black, fontSize: 16, fontWeight: FontWeight.w600),
         ),
-      );
+        onChanged: (String? value) => setState(() {
+          if (value!.isNotEmpty) {
+            this._chosenValue = value;
+            print('set value to: ' + value);
+          }
+        }),
+      ),
+    );
   }
 }
