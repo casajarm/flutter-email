7b070d568f3462a5145828ef5e6822508d0669a6
Greg Jarmiolowski <greg@sqlprompt.net>
2021-10-01 16:04:05 -0400

    fix chosen value and add handler pass down

--- lib/droplist.dart
+++ lib/droplist.dart
@@ -1,50 +1,52 @@
 import 'package:flutter/material.dart';
+
 /* mostly taken from https://medium.com/flutterdevs/dropdown-in-flutter-324ae9caa743 */
 class DropdownList extends StatefulWidget {
-  //String chosenValue;
-  List<String> itemsList;
-  String stateElement;
-  DropdownList(this.itemsList, this.stateElement);
+  DropdownList(this._itemsList, this._chosenValue, this.changeHandler);
+
+  List<String> _itemsList;
+  String? _chosenValue;
+  Function changeHandler;
   @override
-  _DropdownState createState() => _DropdownState(itemsList, stateElement);
+  _DropdownState createState() =>
+      _DropdownState(_itemsList, _chosenValue, changeHandler);
 }
 
 class _DropdownState extends State<DropdownList> {
   List<String> _itemsList;
-  String _chosenValue = '1';
-  String _stateElement;
+  String? _chosenValue;
+  Function changeHandler;
 
-  _DropdownState(this._itemsList, this._stateElement);
+  _DropdownState(this._itemsList, this._chosenValue, this.changeHandler);
+
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: const EdgeInsets.all(0.0),
-        child: DropdownButton<String>(
-          value: _itemsList[0],
-          //elevation: 5,
-          style: TextStyle(color: Colors.black),
+      padding: const EdgeInsets.all(0.0),
+      child: DropdownButton<String>(
+        value: _chosenValue ?? _itemsList[0],
+        //elevation: 5,
+        style: TextStyle(color: Colors.black),
 
-          items: _itemsList.map<DropdownMenuItem<String>>((String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(value),
-            );
-          }).toList(),
-          hint: Text(
-            "Please choose an email account",
-            style: TextStyle(
-                color: Colors.black,
-                fontSize: 16,
-                fontWeight: FontWeight.w600),
-          ),
-          onChanged: (String? value) => setState(() {
-            if (value!.isNotEmpty) {
-              _chosenValue = value;
-              _stateElement = value;
-              print('set value to: ' + value);
-            }
-            }),
+        items: _itemsList.map<DropdownMenuItem<String>>((String value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(value),
+          );
+        }).toList(),
+        hint: Text(
+          "Please choose an email account",
+          style: TextStyle(
+              color: Colors.black, fontSize: 16, fontWeight: FontWeight.w600),
         ),
-      );
+        onChanged: (String? value) => setState(() {
+          if (value!.isNotEmpty) {
+            _chosenValue = value;
+            print('set droplist selected value to: ' + value);
+          }
+          changeHandler(value);
+        }),
+      ),
+    );
   }
 }
