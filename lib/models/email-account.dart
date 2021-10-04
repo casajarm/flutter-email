fb18a2b9d6b3a8a774c65e209919cbbeebb58f43
Greg Jarmiolowski <greg@sqlprompt.net>
2021-10-04 18:04:20 -0400

    finder function

--- lib/models/email-account.dart
+++ lib/models/email-account.dart
@@ -5,3 +5,13 @@ class EmailAccount {
 
   EmailAccount(this.emailAddress, this.password, this.server);
 }
+
+/// Find an email account in the list using emailAddress for search
+EmailAccount findEmailAccountByAddress(
+    List<EmailAccount> emails, String emailAddress) {
+  final EmailAccount emailAccount = emails.firstWhere(
+      (element) => element.emailAddress == emailAddress, orElse: () {
+    return EmailAccount('none', 'none', 'none');
+  });
+  return emailAccount;
+}
